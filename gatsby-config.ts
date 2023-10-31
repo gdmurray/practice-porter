95a3d97936ea6d03a0f9b561e732a32a8d55b31e
Greg Murray <gd-murray@hotmail.com>
2023-10-31 15:30:04 -0700

    website

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,33 +1,33 @@
-import type { GatsbyConfig } from "gatsby";
+import type {GatsbyConfig} from "gatsby";
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `Practice Porter`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "c6POUVF-B2oFZIMG4BvUytIpkZzj1wTG0glgBbc5RwE",
-      "spaceId": "qsfvlzwuzhdr"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-emotion", "gatsby-plugin-google-gtag", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `Practice Porter`,
+        siteUrl: `https://www.practiceporter.ca`
     },
-    __key: "images"
-  }]
+    // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+    // If you use VSCode you can also use the GraphQL plugin
+    // Learn more at: https://gatsby.dev/graphql-typegen
+    graphqlTypegen: true,
+    plugins: [{
+        resolve: 'gatsby-source-contentful',
+        options: {
+            accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+            "spaceId": "qsfvlzwuzhdr"
+        }
+    }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-emotion", "gatsby-plugin-sitemap", {
+        resolve: 'gatsby-plugin-manifest',
+        options: {
+            "icon": "src/images/icon.png"
+        }
+    }, {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+            "name": "images",
+            "path": "./src/images/"
+        },
+        __key: "images"
+    }]
 };
 
 export default config;
